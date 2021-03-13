4194675327ff9751aa18f5794f1b4fd6017815ea
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-03-13 13:54:35 -0700

    first commit

---
+++ src/Components/Welcome/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+        <section>
+            Welcome Page
+        </section>
+    )
+}
+
+export default Welcome;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import Welcome from '../Components/Welcome/Welcome';
+import Skills from '../Components/Skills/Skills';
+import Projects from '../Components/Projects/Projects';
+import Contact from '../Components/Contact/Contact';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <section>
+      <Welcome />
+      <Skills />
+      <Projects />
+      <Contact />
+    </section>
+  )
 }
