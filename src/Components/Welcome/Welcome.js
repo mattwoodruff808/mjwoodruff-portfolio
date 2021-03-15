0b4507b7a957bba43b6251eef887257dd3d3ef7e
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-03-14 22:31:27 -0600

    Finished styling for Welcome component

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -10,18 +10,19 @@ const Welcome = () => {
             <h1>MATT WOODRUFF</h1>
             <h2>Software Engineer</h2>
             <nav>
-                <a href='https://www.linkedin.com/in/matt-j-woodruff/' target='_blank'>
+                <a href='https://www.linkedin.com/in/matt-j-woodruff/' target='_blank' rel='noreferrer'>
                     <img src={linkedinSvg} 
                         alt='LinkedIn Icon' 
                         className='icons' />
                 </a>
-                <a href='https://github.com/mattwoodruff808' target='_blank'>
+                <a href='https://github.com/mattwoodruff808' target='_blank' rel='noreferrer'>
                     <img src={githubSvg} 
                         alt='GitHub Icon' 
                         className='icons' />
                 </a>
                 <a href={pdf} 
                    target='_blank'
+                   rel='noreferrer'
                    className='resume'>View My Resume</a>
             </nav>
         </section>
