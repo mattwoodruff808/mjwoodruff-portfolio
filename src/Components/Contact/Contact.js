4bbda111e5de4f323e222407182db97374819969
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-03-16 00:29:19 -0600

    Finished styling for everything!

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Contact.css'
 
 const Contact = () => {
     return (
-        <section>
-            Contact
+        <section className='Contact'>
+            <h3>Contact Me</h3>
+            <p>I would love to collaborate with you at your company or on your project! My resume has a comprehensive list of my skills and experience. If you find we are a good fit, please reach out!</p>
+            <p>mjwoodruff93@gmail.com</p>
         </section>
     )
 }
