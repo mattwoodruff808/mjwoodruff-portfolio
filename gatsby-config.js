868b83c9422485735f885f0d863ddcd16eb21b67
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-03-16 21:54:45 -0600

    Added meta data!

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,12 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "MattJWoodruff",
+    description:
+      "Personal portfolio site for Matt J. Woodruff. Skills, projects, and contact info available for professional use only.",
+    url: "https://www.mattjwoodruff.com",
+    image: "/static/code-24px.svg",
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import Welcome from '../Components/Welcome/Welcome';
 import Skills from '../Components/Skills/Skills';
 import Projects from '../Components/Projects/Projects';
 import Contact from '../Components/Contact/Contact';
+import Seo from '../Components/Seo';
 import './index.css';
 
 export default function Home() {
   return (
     <section className='App'>
+      <Seo />
       <Welcome />
       <Skills />
       <Projects />
